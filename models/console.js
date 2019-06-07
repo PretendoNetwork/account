b51cb3f1e883e1ccb98d74fe58dc3abbccf047d8
Jonathan Barrow <jonbarrow1998@gmail.com>
2019-06-06 21:28:20 -0400

    Added is_emulator key to console schema

--- models/console.js
+++ models/console.js
@@ -2,6 +2,10 @@ const {Schema, model} = require('mongoose');
 const {DeviceAttributeSchema} = require('./deviceattribute');
 
 const ConsoleSchema = new Schema({
+	is_emulator: {
+		type: Boolean,
+		default: false
+	},
 	console_type: {
 		type: String,
 		enum: ['wup', 'ctr', 'spr', 'ftr', 'ktr', 'red', 'jan'] // wup is WiiU, the rest are the 3DS family. Only wup is used atm
