81dbe2af1eef91e941d8953c10838c5a396c9142
mrjvs <jellevs@gmail.com>
2022-05-08 15:18:23 +0200

    check for correct files in docker entrypoint + elaboration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-files='config.json certs/nex/datastore/secret.key certs/nex/secret.key certs/nex/public.pem'
+# this doesnt check game server specific certs, only static file paths
+files='config.json certs/nex/datastore/secret.key certs/nex/secret.key certs/nex/public.pem certs/access/secret.key certs/access/aes.key certs/access/public.pem'
 
 for file in $files; do
     if [ ! -f $file ]; then
