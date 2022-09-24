0996df985ead53c9e4405578dd7c7955551fb1e9
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-09-24 18:40:30 -0400

    Add encoding option to cache

--- src/cache.js
+++ src/cache.js
@@ -12,11 +12,11 @@ async function setCachedFile(type, name, fileName, value) {
 	await client.set(`${type}:${name}:${fileName}`, value);
 }
 
-async function getCachedFile(type, name, fileName) {
+async function getCachedFile(type, name, fileName, encoding) {
 	const cachedFile = await client.get(`${type}:${name}:${fileName}`);
 
 	if (cachedFile !== null) {
-		return Buffer.from(cachedFile);
+		return Buffer.from(cachedFile, encoding);
 	} else {
 		return cachedFile;
 	}
@@ -24,20 +24,20 @@ async function getCachedFile(type, name, fileName) {
 
 // NEX server cache functions
 
-async function getNEXPublicCert(name) {
-	return await getCachedFile(`nex:${name}:public_cert`);
+async function getNEXPublicCert(name, encoding) {
+	return await getCachedFile(`nex:${name}:public_cert`, encoding);
 }
 
-async function getNEXPrivateCert(name) {
-	return await getCachedFile(`nex:${name}:private_cert`);
+async function getNEXPrivateCert(name, encoding) {
+	return await getCachedFile(`nex:${name}:private_cert`, encoding);
 }
 
-async function getNEXSecretKey(name) {
-	return await getCachedFile(`nex:${name}:secret_key`);
+async function getNEXSecretKey(name, encoding) {
+	return await getCachedFile(`nex:${name}:secret_key`, encoding);
 }
 
-async function getNEXAESKey(name) {
-	return await getCachedFile(`nex:${name}:aes_key`);
+async function getNEXAESKey(name, encoding) {
+	return await getCachedFile(`nex:${name}:aes_key`, encoding);
 }
 
 async function setNEXPublicCert(name, value) {
@@ -58,20 +58,20 @@ async function setNEXAESKey(name, value) {
 
 // 3rd party service cache functions
 
-async function getServicePublicCert(name) {
-	return await getCachedFile(`service:${name}:public_cert`);
+async function getServicePublicCert(name, encoding) {
+	return await getCachedFile(`service:${name}:public_cert`, encoding);
 }
 
-async function getServicePrivateCert(name) {
-	return await getCachedFile(`service:${name}:private_cert`);
+async function getServicePrivateCert(name, encoding) {
+	return await getCachedFile(`service:${name}:private_cert`, encoding);
 }
 
-async function getServiceSecretKey(name) {
-	return await getCachedFile(`service:${name}:secret_key`);
+async function getServiceSecretKey(name, encoding) {
+	return await getCachedFile(`service:${name}:secret_key`, encoding);
 }
 
-async function getServiceAESKey(name) {
-	return await getCachedFile(`service:${name}:aes_key`);
+async function getServiceAESKey(name, encoding) {
+	return await getCachedFile(`service:${name}:aes_key`, encoding);
 }
 
 async function setServicePublicCert(name, value) {
