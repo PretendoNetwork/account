625bab0e25e6eaeefcc03ec8702dfa1a6f0cc3f8
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-10-08 09:56:17 -0400

    Added loading config from env

--- src/config-manager.js
+++ src/config-manager.js
@@ -1,6 +1,8 @@
 const fs = require('fs-extra');
 const logger = require('../logger');
 
+require('dotenv').config();
+
 /**
  * @typedef {Object} Config
  * @property {object} http HTTP server settings
@@ -16,7 +18,7 @@ const logger = require('../logger');
  * @property {string} email.host SMTP server address
  * @property {number} email.port SMTP server port
  * @property {boolean} email.secure Secure SMTP
- * @property {string} email.from Email "from" name/address
+ * @property {string} email.from Email 'from' name/address
  * @property {object} email.auth Email authentication settings
  * @property {string} email.auth.user Email username
  * @property {string} email.auth.pass Email password
@@ -36,12 +38,60 @@ const logger = require('../logger');
 let config = {};
 
 function configure() {
-	if (!fs.pathExistsSync(`${__dirname}/../config.json`)) {
-		logger.error('Failed to locate config.json file');
-		process.exit(0);
-	}
+	if (process.env.PN_ACT_PREFER_ENV_CONFIG === 'true') {
+		logger.info('Loading config from env');
 
-	config = require(`${__dirname}/../config.json`);
+		config = {
+			http: {
+				port: Number(process.env.PN_ACT_CONFIG_HTTP_PORT)
+			},
+			mongoose: {
+				uri: process.env.PN_ACT_CONFIG_MONGO_URI,
+				database: process.env.PN_ACT_CONFIG_MONGO_DB_NAME,
+				options: Object.keys(process.env)
+					.filter(key => key.startsWith('PN_ACT_CONFIG_MONGOOSE_OPTION_'))
+					.reduce((obj, key) => {
+						obj[key.split('_').pop()] = process.env[key];
+						return obj;
+					}, {})
+			},
+			redis: {
+				client: {
+					url: process.env.PN_ACT_CONFIG_REDIS_URL
+				}
+			},
+			email: {
+				host: process.env.PN_ACT_CONFIG_EMAIL_HOST,
+				port: Number(process.env.PN_ACT_CONFIG_EMAIL_PORT),
+				secure: Boolean(process.env.PN_ACT_CONFIG_EMAIL_SECURE),
+				auth: {
+					user: process.env.PN_ACT_CONFIG_EMAIL_USERNAME,
+					pass: process.env.PN_ACT_CONFIG_EMAIL_PASSWORD
+				},
+				from: process.env.PN_ACT_CONFIG_EMAIL_FROM
+			},
+			aws: {
+				spaces: {
+					key: process.env.PN_ACT_CONFIG_S3_ACCESS_KEY,
+					secret: process.env.PN_ACT_CONFIG_S3_ACCESS_SECRET
+				}
+			},
+			hcaptcha: {
+				secret: process.env.PN_ACT_CONFIG_HCAPTCHA_SECRET
+			},
+			cdn_base: process.env.PN_ACT_CONFIG_CDN_BASE,
+			website_base: process.env.PN_ACT_CONFIG_WEBSITE_BASE
+		};
+	} else {
+		logger.info('Loading config from config.json');
+
+		if (!fs.pathExistsSync(`${__dirname}/../config.json`)) {
+			logger.error('Failed to locate config.json file');
+			process.exit(0);
+		}
+
+		config = require(`${__dirname}/../config.json`);
+	}
 
 	module.exports.config = config;
 }
