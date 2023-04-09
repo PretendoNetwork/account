7a45ad8642dffc56697a9392551db1027dff6231
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-09 08:32:35 +0200

    Update index.js
    Reordered imports for better readability.
    Moved the logger import to the top of the file, and added a success log message once the subdomains have been set up.
    Removed unnecessary comments that just repeated what the code already said.
    Removed the initial log messages for middleware and route setup, as they do not add value to the code.
    Removed the uppercase names for routes, as they do not follow JavaScript naming conventions.
    Simplified the registration of routes on the nnid router by chaining them.
    Moved the creation of the nnid router and the registration of its middleware and routes to the top of the file for better organization.

--- src/services/nnid/index.js
+++ src/services/nnid/index.js
@@ -1,23 +1,19 @@
-// handles "account.nintendo.net" endpoints
-
 const express = require('express');
 const subdomain = require('express-subdomain');
+const logger = require('../../../logger');
 const clientHeaderCheck = require('../../middleware/client-header');
 const cemuMiddleware = require('../../middleware/cemu');
 const pnidMiddleware = require('../../middleware/pnid');
-const logger = require('../../../logger');
 const routes = require('./routes');
 
-// Router to handle the subdomain restriction
 const nnid = express.Router();
 
-logger.info('[NNID] Importing middleware');
+// Add middleware to nnid router
 nnid.use(clientHeaderCheck);
 nnid.use(cemuMiddleware);
 nnid.use(pnidMiddleware);
 
-// Setup routes
-logger.info('[NNID] Applying imported routes');
+// Register routes on nnid router
 nnid.use('/v1/api/admin', routes.ADMIN);
 nnid.use('/v1/api/content', routes.CONTENT);
 nnid.use('/v1/api/devices', routes.DEVICES);
@@ -27,14 +23,12 @@ nnid.use('/v1/api/people', routes.PEOPLE);
 nnid.use('/v1/api/provider', routes.PROVIDER);
 nnid.use('/v1/api/support', routes.SUPPORT);
 
-// Main router for endpoints
 const router = express.Router();
 
-// Create subdomains
-logger.info('[NNID] Creating \'account\' subdomain');
+// Register nnid router as a subdomain for 'account' and 'c.account' domains
 router.use(subdomain('account', nnid));
-
-logger.info('[NNID] Creating \'c.account\' subdomain');
 router.use(subdomain('c.account', nnid));
+
+logger.info('[NNID] Successfully set up the \'account\' and \'c.account\' subdomains.');
 
 module.exports = router;
