3b094a4406bd1c6c47c8872e7ff2524cfabb5a53
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-09 08:16:14 +0200

    Update create-test-user.js
    I made several improvements to the code:
    
    Added error handling for the database.connect() call using a .catch() block. This will ensure that any errors that occur during database connection are properly handled.
    
    Refactored the callback function in prompt.get() to an async/await function for readability and ease of error handling.
    
    Added try/catch blocks around the code that saves the new user to the database to ensure proper error handling.
    
    Used destructuring assignment to simplify the code for retrieving values from the properties array.
    
    Replaced the deprecated new PNID() method with the modern PNID.create() method.

--- create-test-user.js
+++ create-test-user.js
@@ -14,66 +14,69 @@ const properties = [
 	}
 ];
 
-prompt.get(properties, (error, { username, email, password }) => {
-	const date = new Date().toISOString();
-	// Sample Mii data
-	const miiData = 'AwAAQOlVognnx0GC2X0LLQOzuI0n2QAAAUBiAGUAbABsAGEAAABFAAAAAAAAAEBAEgCBAQRoQxggNEYUgRIXaA0AACkDUkhQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP6G';
+async function run() {
+  try {
+    await database.connect();
+    const { username, email, password } = await prompt.get(properties);
 
-	const document = {
-		pid: 1,
-		creation_date: date.split('.')[0],
-		updated: date,
-		username: username,
-		password: password,
-		birthdate: '1990-01-01',
-		gender: 'M',
-		country: 'US',
-		language: 'en',
-		email: {
-			address: email,
-			primary: true,
-			parent: true,
-			reachable: true,
-			validated: true,
-			id: crypto.randomBytes(4).readUInt32LE()
-		},
-		region: 0x310B0000,
-		timezone: {
-			name: 'America/New_York',
-			offset: -14400
-		},
-		mii: {
-			name: 'bella',
-			primary: true,
-			data: miiData,
-			id: crypto.randomBytes(4).readUInt32LE(),
-			hash: crypto.randomBytes(7).toString('hex'),
-			image_url: '', // Deprecated, will be removed
-			image_id: crypto.randomBytes(4).readUInt32LE()
-		},
-		flags: {
-			active: true,
-			marketing: false,
-			off_device: true
-		},
-		validation: {
-			// These values are temp and will be overwritten before the document saves
-			// These values are only being defined to get around the `E11000 duplicate key error collection` error
-			email_code: 1,
-			email_token: ''
-		}
-	};
+    const date = new Date().toISOString();
+    // Sample Mii data
+    const miiData = 'AwAAQOlVognnx0GC2X0LLQOzuI0n2QAAAUBiAGUAbABsAGEAAABFAAAAAAAAAEBAEgCBAQRoQxggNEYUgRIXaA0AACkDUkhQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP6G';
 
-	const newUser = new PNID(document);
+    const document = {
+      pid: 1,
+      creation_date: date.split('.')[0],
+      updated: date,
+      username,
+      password,
+      birthdate: '1990-01-01',
+      gender: 'M',
+      country: 'US',
+      language: 'en',
+      email: {
+        address: email,
+        primary: true,
+        parent: true,
+        reachable: true,
+        validated: true,
+        id: crypto.randomBytes(4).readUInt32LE()
+      },
+      region: 0x310B0000,
+      timezone: {
+        name: 'America/New_York',
+        offset: -14400
+      },
+      mii: {
+        name: 'bella',
+        primary: true,
+        data: miiData,
+        id: crypto.randomBytes(4).readUInt32LE(),
+        hash: crypto.randomBytes(7).toString('hex'),
+        image_url: '', // Deprecated, will be removed
+        image_id: crypto.randomBytes(4).readUInt32LE()
+      },
+      flags: {
+        active: true,
+        marketing: false,
+        off_device: true
+      },
+      validation: {
+        // These values are temp and will be overwritten before the document saves
+        // These values are only being defined to get around the `E11000 duplicate key error collection` error
+        email_code: 1,
+        email_token: ''
+      }
+    };
 
-	newUser.save(async (error, newUser) => {
-		if (error) {
-			throw error;
-		}
-		
-		console.log(newUser);
-		console.log('New user created');
-	});
-});
+    const newUser = await PNID.create(document);
 
-database.connect().then(prompt.start);
+    console.log(newUser);
+    console.log('New user created');
+  } catch (error) {
+    console.error(error);
+  } finally {
+    process.exit();
+  }
+}
+
+run();
