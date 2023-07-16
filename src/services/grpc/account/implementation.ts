3844531191b38fc6b0e3dfaf780b8d0c15629b59
Daniel López Guimaraes <danielectra@outlook.com>
2023-07-17 00:20:26 +0100

    Add GetNEXData GRPC method
    Returns all details of a NEX account.

--- src/services/grpc/account/implementation.ts
+++ src/services/grpc/account/implementation.ts
@@ -1,8 +1,10 @@
 import { AccountServiceImplementation } from 'pretendo-grpc-ts/dist/account/account_service';
 import { getUserData } from '@/services/grpc/account/get-user-data';
 import { getNEXPassword } from '@/services/grpc/account/get-nex-password';
+import { getNEXData } from '@/services/grpc/account/get-nex-data';
 
 export const accountServiceImplementation: AccountServiceImplementation = {
 	getUserData,
-	getNEXPassword
+	getNEXPassword,
+	getNEXData
 };
