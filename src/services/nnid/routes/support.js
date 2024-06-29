575c9b9c9087af7ccc9ff4d790b608a0954b1821
Joshua Peisach <itzswirlz2020@outlook.com>
2024-06-29 16:26:37 -0400

    support: implement send_confirmation/pin/:email

--- src/services/nnid/routes/support.js
+++ src/services/nnid/routes/support.js
@@ -118,6 +118,33 @@ router.get('/resend_confirmation', async (request, response) => {
 
 /**
  * [GET]
+ * Replacement for: https://account.nintendo.net/v1/api/support/send_confirmation/pin/:email/
+ * Description: Sends a users confirmation email
+ */
+router.get('/send_confirmation/pin/:email/', async (request, response) => {
+	const { email } = request.params;
+
+	const pnid = await database.getUserByEmailAddress(email);
+
+	if (!pnid) {
+		// TODO - Unsure if this is the right error
+		return response.status(400).send(xmlbuilder.create({
+			errors: {
+				error: {
+					code: '0130',
+					message: 'PID has not been registered yet'
+				}
+			}
+		}).end());
+	}
+
+	await util.sendConfirmationEmail(pnid);
+
+	response.status(200).send('');
+});
+
+/**
+ * [GET]
  * Replacement for: https://account.nintendo.net/v1/api/support/forgotten_password/PID
  * Description: Sends the user a password reset email
  * NOTE: On NN this was a temp password that expired after 24 hours. We do not do that
