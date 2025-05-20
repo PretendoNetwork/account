d54ee06de1f8818988878d1eea99c656343dcbb0
William Oldham <github@binaryoverload.co.uk>
2025-05-20 23:40:09 +0100

    fix: pin mii-js at working version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,5 @@ export default [
 				...globals.node
 			}
 		}
-	},
-	{
-		rules: {
-			'import/no-unresolved': ['error', {
-				ignore: [
-					'mii-js' // mii-js is a weird package that doesn't have a proper module resolution - it's from GitHub
-				]
-			}]
-		}
 	}
 ];
